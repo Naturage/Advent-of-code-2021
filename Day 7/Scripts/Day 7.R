7d4b54e3bf5b4830a21e43c6e5fb8f9f31a0f8c9
Naturage <54602845+Naturage@users.noreply.github.com>
2021-12-07 12:36:33 +0000

    Day 7

---
+++ Day 7/Scripts/Day 7.R
@@ -0,0 +1,25 @@
+source(here::here("common functions.R"))
+
+day <- 7
+
+# Data input
+input <- get_input(day) %>% strsplit(split = ",") %>% unlist() %>% as.numeric()
+
+# Pt 1: I know by heart optimum is at a median for linear error. Also if it's not integer we're still god - any number between two medians will do.
+(sum(abs(input - median(input))))
+
+# Pt 2: this is almost quadratic error so has to be near mean in median's direction.
+mean <- mean(input)
+median <- median(input)
+direction <- sign(median - mean)
+
+score <- vector()
+
+for (i in 0:5){
+  guess <- ceiling(mean - direction) + i * direction
+  score[i+1] = sum(abs(input - guess) + (input - guess)**2)/2
+}
+
+(min(score))
+
+get_input(7) %>% strsplit(split = ",") %>% unlist() %>% as.numeric() %>% {. - median(.)} %>% abs() %>% sum()
